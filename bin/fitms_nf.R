ffb9fcddf134b89aad24322b37a1bf11ea81a903
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-10-27 09:32:00 +0100

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -2,59 +2,49 @@
 library(VariantAnnotation)
 library(signature.tools.lib)
 args = commandArgs(trailingOnly=TRUE)
+
 sample <- args[1]
-mtr_input <- args[2]
-#fileConn<-file("output.txt")
-#writeLines(c(sample), fileConn)
-#close(fileConn)
+all <- args[2]
+clonal_any <- args[3]
+clonal_early <- args[4]
+clonal_late <- args[5]
+subclonal <- args[6]
 
-#read in the input file to mutationtimer
-tab  <- read.table(mtr_input, sep='\t')
-#change the format of the input file to that of FitMS
-tab<- tab[,c(1,2,4,5)]
-names(tab)[names(tab) == "V1"] <- "chr"
-names(tab)[names(tab) == "V2"] <- "position"
-names(tab)[names(tab) == "V4"] <- "REF"
-names(tab)[names(tab) == "V5"] <- "ALT"
+clonality_in <- c(all, clonal_any, clonal_early, clonal_late, subclonal)
+clonality <- list()
 
-chroms <- c("1",  "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "2",  "20", "21", "22", "3",  "4" , "5", "6",  "7",  "8","9" ,"X", "Y")
-
-
-for(i in 1:nrow(tab)){
-  if(is.element(tab$chr[i], chroms) != TRUE){
-    tab <- tab[-c(i), ]
+for(state in clonality_in){
+  if(state != 'False'){
+    clonality <- append(clonality, state)
   }
 }
-#to_delete <- list()
-#for(i in 1:length(tab)){
-#  if(is.na(suppressWarnings(as.numeric(as.character(tab$chr[i])))) == TRUE){
-#  to_delete <- append(to_delete, i)
-#  }
-#}
-#tab = tab[to_delete,]
-#tab <- tab[ complete.cases( sapply( tab$chr, strtoi ) ), ]
 
-#write.csv(tab, paste0(sample,'_fitms_input.csv'))
+counter2 = 0
+for (state in clonality){
+  counter2 = counter2 +1
+  filenameinput = state
+  tab <- read.table(filenameinput, sep='\t')
+  names(tab) <- tab[1,]
+  tab <- tab[-1,]
+  rownames(tab) <-NULL
+  tab$position <- as.numeric(tab$position)
+  res <- tabToSNVcatalogue(tab, genome.v)
+  if(counter2 == 1){
+    df <- data.frame(res$catalogue)
+  }
+  else
+    df[paste0(sample, '_', state)] <- res$catalogue
+}
 
-#optinal read in and out tab
-#tab  <- write.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
-#tab  <- read.table('C:/Users/Danie/Documents/scanb/MTR/PD31029a/input/PD31029a_caveman_MTR.txt')
+write.csv(df, paste0(sample, '_clonality_state_catalogue.csv'))
 
-organ = "Breast"
-genome.v  ="hg38"
-#print(head(tab))
-res <- tabToSNVcatalogue(tab, genome.v)
-df <- data.frame(res$catalogue)
-names(df)[names(df) == "catalogue"] <- sample
-write.csv(df, paste0(sample,'_catalogue.csv'))
 
-res <-FitMS(catalogues = df, 
-           organ =organ, 
-           exposureFilterType="giniScaledThreshold",
-           useBootstrap = TRUE, 
-           nboot = 200)
+plotSubsSignatures(signature_data_matrix = df,output_file = paste0(corepath, sample, "_SNV_catalogues.pdf"))
 
-plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
+res <-FitMS(catalogues = catalogue1, 
+            organ =organ, 
+            exposureFilterType="giniScaledThreshold",
+            useBootstrap = TRUE, 
+            nboot = 200)
 
-#write.csv(res$exposures, 'exposures.tsv', sep='\t')
 plotFitMS(res, 'results/')
