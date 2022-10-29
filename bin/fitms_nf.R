5d9f187f9ae33bc30ac42c8b832378bbae71efd0
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2022-10-29 13:26:21 +0100

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -19,41 +19,55 @@ clonality_print <- list()
 clonality <- list()
 
 for(state in 1:length(clonality_in)){
-  if(state != 'False'){
+  if(clonality_in[state] != 'False'){
     clonality <- append(clonality, clonality_in[state])
     state_print <- deparse(clonality_in[state])
     clonality_print <- append(clonality_print, state_print)
   }
 }
 
+fileConn<-file("clonality.txt")
+writeLines(clonality, fileConn)
+close(fileConn)
 
-for (state in 1:length(clonality)){
-  filenameinput = clonality[state]
-  tab <- read.table(filenameinput, sep='\t')
-  names(tab) <- tab[1,]
-  tab <- tab[-1,]
-  rownames(tab) <-NULL
-  tab$position <- as.numeric(tab$position)
-  res <- tabToSNVcatalogue(tab, genome.v)
-  if(counter == 0){
-    df <- data.frame(res$catalogue)
-    names(df)[names(df) == 'catalogue'] <- paste0(sample, '_',clonality_print[state])
-  }
-  else{
-    df$'placeholder' <- res$catalogue$catalogue
-    names(df)[names(df) == 'placeholder'] <- paste0(sample, '_',clonality_print[state])
-    }
-}
-
-write.csv(df, paste0(sample, '_clonality_state_catalogue.csv'))
+fileConn<-file("clonality_print.txt")
+writeLines(clonality_print, fileConn)
+close(fileConn)
 
 
-plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
+fileConn<-file("clonality_in.txt")
+writeLines(clonality_in, fileConn)
+close(fileConn)
 
-res <-FitMS(catalogues = df,
-            organ =organ, 
-            exposureFilterType="giniScaledThreshold",
-            useBootstrap = TRUE, 
-            nboot = 200)
 
-plotFitMS(res, 'results/')
+
+#for (state in 1:length(clonality)){
+#  filenameinput = clonality[state]
+#  tab <- read.table(filenameinput, sep='\t')
+#  names(tab) <- tab[1,]
+#  tab <- tab[-1,]
+#  rownames(tab) <-NULL
+#  tab$position <- as.numeric(tab$position)
+ ## res <- tabToSNVcatalogue(tab, genome.v)
+ # if(counter == 0){
+## #   df <- data.frame(res$catalogue)
+#    names(df)[names(df) == 'catalogue'] <- paste0(sample, '_',clonality_print[state])
+#  }
+#  else{
+#    df$'placeholder' <- res$catalogue$catalogue
+#    names(df)[names(df) == 'placeholder'] <- paste0(sample, '_',clonality_print[state])
+#    }
+#}
+
+#write.csv(df, paste0(sample, '_clonality_state_catalogue.csv'))
+##
+#
+#plotSubsSignatures(signature_data_matrix = df,output_file = paste0(sample, "_SNV_catalogues.pdf"))
+#
+#res <-FitMS(catalogues = df,
+ #           organ =organ, 
+  #          exposureFilterType="giniScaledThreshold",
+   #         useBootstrap = TRUE, 
+    #        nboot = 200)
+
+#plotFitMS(res, 'results/')
